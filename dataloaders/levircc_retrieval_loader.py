96b1ea088e9c800c69b1d86ce15a5a366959a402
bedylmz <bedylmz03@gmail.com>
2025-10-31 13:27:37 +0300

    changed default features path

--- dataloaders/levircc_retrieval_loader.py
+++ dataloaders/levircc_retrieval_loader.py
@@ -49,8 +49,8 @@ class LEVIRCC_DataLoader(Dataset):
 
         self.subset = subset
         self.data_path = data_path
-        self.default_features_path = os.path.join(self.data_path, "train")
-        #self.default_features_path = os.path.join(self.data_path, "sem" ,"train")
+        self.default_features_path = os.path.join(self.data_path, "train", "rgb")
+        self.semantic_default_features_path = os.path.join(self.data_path, "train" ,"sem")
 
         if tokenizer is not None:
             self.tokenizer = tokenizer
