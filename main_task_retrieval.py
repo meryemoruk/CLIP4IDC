a801afd8cc8d61c31657593503cb863580816221
bedylmz <bedylmz03@gmail.com>
2025-11-06 14:54:52 +0300

    changed for single gpu

--- main_task_retrieval.py
+++ main_task_retrieval.py
@@ -172,11 +172,20 @@ def set_seed_logger(args):
     return args
 
 
-def init_device(args):
+def init_device(args, local_rank):
     global logger
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    return device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu", local_rank)
+
+    n_gpu = torch.cuda.device_count()
+    logger.info("device: {} n_gpu: {}".format(device, n_gpu))
+    args.n_gpu = n_gpu
+
+    if args.batch_size % args.n_gpu != 0 or args.batch_size_val % args.n_gpu != 0:
+        raise ValueError("Invalid batch_size/batch_size_val and n_gpu parameter: {}%{} and {}%{}, should be == 0".format(
+            args.batch_size, args.n_gpu, args.batch_size_val, args.n_gpu))
+
+    return device, n_gpu
 
 
 def init_model(args, device, n_gpu, local_rank):
