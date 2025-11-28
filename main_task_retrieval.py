da1f2da2520791d77fc402f81567e8edfb372456
bedylmz <bedylmz03@gmail.com>
2025-11-28 14:13:36 +0300

    result debug

--- main_task_retrieval.py
+++ main_task_retrieval.py
@@ -499,7 +499,14 @@ def _run_on_single_gpu_retrieval(
         b1b2_logits = b1b2_logits.cpu().detach().numpy()
         result.append(b1b2_logits)
 
-    """result_tensor = torch.tensor(result)
+    result_np = np.array(result)
+
+    # 2. flatten() ile tüm iç içe parantezleri kaldırıp tek bir düz çizgi haline getiriyoruz
+    # Yeni boyut: (6121,) olacak. Artık dümdüz bir sayı dizisi.
+    result_flat = result_np.flatten()
+
+    # 3. Şimdi Tensor'a çevirip Top-K işlemini yapabiliriz
+    result_tensor = torch.from_numpy(result_flat)
     # 1. Adım: argsort ile sıralama yapıldığında elemanların nereye gideceğini (indeksleri) bulur.
     # Bu küçükten büyüğe sıralar, o yüzden sonuna dilimleme ekleriz.
     top_5_degerler, top_5_indeksler = torch.topk(result_tensor, k=5)
@@ -509,7 +516,7 @@ def _run_on_single_gpu_retrieval(
     top_5_indeks_listesi = top_5_indeksler.cpu().numpy().tolist()
 
     print(f"Top 5 Değerler: {top_5_deger_listesi}")
-    print(f"Top 5 İndeksler: {top_5_indeks_listesi}")"""
+    print(f"Top 5 İndeksler: {top_5_indeks_listesi}")
 
 
     return result
